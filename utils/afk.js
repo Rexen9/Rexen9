5a42b7df72a95e9ad43f84e63e2347968fec5656
Rexen9 <itsammar6251@gmail.com>
2025-09-27 15:11:27 +0500

    Delete utils directory

--- utils/afk.js
+++
@@ -1,8 +0,0 @@
-
-const { Collection } = require('discord.js');
-
-const afk = new Collection();
-const globalAfk = new Collection();
-const afkPings = new Collection(); // Stores pings for AFK users
-
-module.exports = { afk, globalAfk, afkPings };
